f2de73d08595cf026ff7c05e84b8f2fba7309c5f
Vosa <vosagh82@gmail.com>
2019-05-14 12:09:49 +0300

    tets4

---
+++ test.ps1
@@ -0,0 +1,26 @@
+
+for ($i = 2; $i -lt 10; $i++)
+{
+    $ip="10.88.20.$i"
+
+
+    #ping
+
+    $ipStatus=Test-Connection -ComputerName $ip -Quiet -Count 1
+
+    #exit if not ping
+
+    if ($ipStatus -eq $False)   {continue} # {break} # {write-host "pc disabled"} breack
+
+    # telnte smb port 445
+
+
+    Write-Host $ip
+
+    #start service
+
+    get-service -computername @($ip) | where {$_.name -eq 'SepMasterService'} | format-table Name,Status,Machinename -autosize
+
+    #Test-NetConnection -Port 445 -ComputerName "$ip"
+
+}
