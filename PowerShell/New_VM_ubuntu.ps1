6b3e532283b0d9cda61707563eee7ed297d0072f
Your Name <you@example.com>
2021-05-05 22:17:39 +0300

    1

--- PowerShell/New_VM_ubuntu.ps1
+++ PowerShell/New_VM_ubuntu.ps1
@@ -1,5 +1,5 @@
 ﻿
-$name_vm = 'ubuntu20-Dev'
+$name_vm = 'ubuntu20t1-wordpress'
 $path_vm = 'D:\Hyper-V\linux\'
 #$path_iso = 'D:\soft\iso\windows\Windows Server\Windows Server 2012\SW_DVD5_Win_Svr_Std_and_DataCtr_2012_64Bit_Russian_Core_MLF_X18-27640.ISO'
 $vhd_vm = $path_vm + '\' + $name_vm  + '\'+ $name_vm + '.vhdx'
